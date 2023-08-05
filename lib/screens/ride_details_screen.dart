483ea35dc9dca08fd91c9d00335001f6af3833cf
Ana Skarica <ana.skarica.as@gmail.com>
2023-08-05 13:29:19 +0200

    [ADD] other riders UI

--- lib/screens/ride_details_screen.dart
+++ lib/screens/ride_details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:bikerzone/models/ride.dart';
 import 'package:bikerzone/models/user.dart';
+import 'package:bikerzone/widgets/large_button_custom.dart';
+import 'package:bikerzone/widgets/stop_points_custom.dart';
 import 'package:bikerzone/widgets/top_navigation_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -13,26 +15,6 @@ class RideDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final List<Map<String, dynamic>> itemsList;
-    itemsList = [
-      {"icon": Icons.location_on, "text": ride.exactStartingPoint},
-      {
-        "icon": Icons.add_road,
-        "text": ride.highway ? "Putujemo autocestom" : "Ne putujemo autocestom"
-      },
-      {
-        "icon": Icons.two_wheeler,
-        "text": ride.acceptType == "Svi motori"
-            ? "Prihvaćamo sve tipove motora"
-            : "Preferiramo tip ${ride.acceptType}"
-      },
-      {"icon": Icons.info_outline, "text": ride.exactStartingPoint},
-      {
-        "icon": Icons.people_alt,
-        "text": "Max broj ljudi u grupi: ${ride.maxPeople}"
-      },
-      {"icon": Icons.speed, "text": ride.pace},
-    ];
 
     return Scaffold(
       backgroundColor: const Color(0xFFEAF2F4),
@@ -46,178 +28,13 @@ class RideDetailsScreen extends StatelessWidget {
                   rightIcon: null),
               DetailsCard1(screenWidth: screenWidth, ride: ride, user: user),
               DetailsCard2(screenWidth: screenWidth, ride: ride, user: user),
-              DetailsCard3(screenWidth: screenWidth, itemsList: itemsList),
+              DetailsCard3(screenWidth: screenWidth, ride: ride),
+              StopPointsCustom(screenWidth: screenWidth, ride: ride),
+              DetailsCard4(screenWidth: screenWidth),
               Container(
-                margin: const EdgeInsets.all(20),
-                width: screenWidth * 0.9,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.3),
-                      blurRadius: 4,
-                      offset: const Offset(2, 4),
-                    )
-                  ],
-                ),
-                child: Stack(
-                  children: [
-                    Container(
-                      height: ride.stopPoints.length * 50 + 170,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: AssetImage("lib/images/road-background.jpg"),
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: screenWidth * 0.25,
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFF9B0B0),
-                            borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(16),
-                                bottomRight: Radius.circular(16)),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.3),
-                                blurRadius: 4,
-                                offset: const Offset(2, 4),
-                              )
-                            ],
-                          ),
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 15, vertical: 10),
-                            child: Row(
-                              children: [
-                                Icon(Icons.public, color: Color(0xFFA41723)),
-                                SizedBox(width: 5),
-                                Text(
-                                  "Ruta:",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w400,
-                                    color: Color(0xFF3F3F3F),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(15),
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  const Icon(Icons.emoji_flags,
-                                      color: Color(0xFF3F3F3F)),
-                                  const SizedBox(width: 5),
-                                  Text(
-                                    ride.startingPoint,
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w400,
-                                      color: Color(0xFF3F3F3F),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: ride.stopPoints.length * 52,
-                                child: ListView.builder(
-                                  physics: const NeverScrollableScrollPhysics(),
-                                  itemCount: ride.stopPoints.length,
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    return Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          const Row(
-                                            children: [
-                                              Padding(
-                                                padding: EdgeInsets.only(
-                                                    left: 5, bottom: 6, top: 2),
-                                                child: Text(
-                                                  " |",
-                                                  style: TextStyle(
-                                                    fontSize: 16,
-                                                    fontWeight: FontWeight.w400,
-                                                    color: Color(0xFF3F3F3F),
-                                                  ),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                          Padding(
-                                            padding:
-                                                const EdgeInsets.only(left: 4),
-                                            child: Row(
-                                              children: [
-                                                const Icon(
-                                                  Icons.adjust,
-                                                  size: 16,
-                                                ),
-                                                const SizedBox(width: 10),
-                                                Text(
-                                                  ride.stopPoints[index],
-                                                  style: const TextStyle(
-                                                    fontSize: 16,
-                                                    fontWeight: FontWeight.w400,
-                                                    color: Color(0xFF3F3F3F),
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          )
-                                        ]);
-                                  },
-                                ),
-                              ),
-                              const Row(
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.only(
-                                        left: 5, bottom: 6, top: 2),
-                                    child: Text(
-                                      " |",
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w400,
-                                        color: Color(0xFF3F3F3F),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  const Icon(Icons.emoji_events_outlined,
-                                      color: Color(0xFF3F3F3F)),
-                                  const SizedBox(width: 5),
-                                  Text(
-                                    ride.finishingPoint,
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w400,
-                                      color: Color(0xFF3F3F3F),
-                                    ),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                child: const LargeButtonCustom(
+                    onTap: null, btnText: "Pridruži se ovoj vožnji"),
               )
             ],
           ),
@@ -471,14 +288,33 @@ class DetailsCard3 extends StatelessWidget {
   const DetailsCard3({
     super.key,
     required this.screenWidth,
-    required this.itemsList,
+    required this.ride,
   });
-
+  final Ride ride;
   final double screenWidth;
-  final dynamic itemsList;
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> itemsList;
+    itemsList = [
+      {"icon": Icons.location_on, "text": ride.exactStartingPoint},
+      {
+        "icon": Icons.add_road,
+        "text": ride.highway ? "Putujemo autocestom" : "Ne putujemo autocestom"
+      },
+      {
+        "icon": Icons.two_wheeler,
+        "text": ride.acceptType == "Svi motori"
+            ? "Prihvaćamo sve tipove motora"
+            : "Preferiramo tip ${ride.acceptType}"
+      },
+      {"icon": Icons.info_outline, "text": ride.exactStartingPoint},
+      {
+        "icon": Icons.people_alt,
+        "text": "Max broj ljudi u grupi: ${ride.maxPeople}"
+      },
+      {"icon": Icons.speed, "text": ride.pace},
+    ];
     return Container(
       height: 250,
       width: screenWidth * 0.9,
@@ -530,6 +366,156 @@ class DetailsCard3 extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class DetailsCard4 extends StatelessWidget {
+  const DetailsCard4({super.key, required this.screenWidth});
+  final double screenWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> helpRiders;
+    helpRiders = [
+      {
+        "name": "JHorvat123",
+        "bike": "BMW R 1250 GS",
+        "image": "lib/images/no_image.jpg"
+      },
+      {
+        "name": "JTest123",
+        "bike": "Honda CBR 1500",
+        "image": "lib/images/helmet.png"
+      },
+      {
+        "name": "JMIki123",
+        "bike": "Tomos APN 6S",
+        "image": "lib/images/no_image.jpg"
+      }
+    ];
+    return Container(
+      width: screenWidth * 0.9,
+      decoration: BoxDecoration(
+        color: const Color(0xFFEEEEEE),
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            blurRadius: 4,
+            offset: const Offset(2, 4),
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 200,
+            decoration: BoxDecoration(
+              color: const Color(0xFF444444),
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  blurRadius: 4,
+                  offset: const Offset(2, 4),
+                )
+              ],
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Row(
+                children: [
+                  Icon(Icons.emoji_people, color: Color(0xFFEAEAEA)),
+                  Icon(Icons.two_wheeler, color: Color(0xFFEAEAEA)),
+                  SizedBox(width: 10),
+                  Text(
+                    "Ostali vozači:",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFFEAEAEA),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: helpRiders.length * 91,
+            child: ListView.builder(
+                itemCount: helpRiders.length,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          right: 0,
+                          child: Container(
+                            height: 70,
+                            alignment: Alignment.centerLeft,
+                            width: screenWidth * 0.7,
+                            decoration: BoxDecoration(
+                                color: const Color(0xFFEAEAEA),
+                                border: Border.all(
+                                    color: const Color(0xFF0276B4), width: 2),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: const Color(0xFF0276B4)
+                                        .withOpacity(0.5),
+                                    blurRadius: 4,
+                                    offset: const Offset(0, 4),
+                                  )
+                                ],
+                                borderRadius: BorderRadius.circular(16)),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 35),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    helpRiders[index]['name'],
+                                    style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600,
+                                        color: Color(0xFF444444)),
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.two_wheeler,
+                                        size: 24,
+                                        color: Color(0xFF0276B4),
+                                      ),
+                                      const SizedBox(width: 5),
+                                      Text(helpRiders[index]['bike']),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        CircleAvatar(
+                          radius: 35,
+                          backgroundImage:
+                              AssetImage(helpRiders[index]['image']),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+          )
+        ],
       ),
     );
   }
